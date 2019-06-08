3d9055c9009115a8015fbdbcf415a732c66a910f
Pradyumna Newalkar <contactpradyumna1@gmail.com>
2019-06-08 06:21:35 +0000

    Updates

---
+++ precache-manifest.f31a9f92f3da39dfbc88bc4e1286cb8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "803aeeb27250b23dcd33ecf1b115dd2d",
+    "url": "/snake/index.html"
+  },
+  {
+    "revision": "07395b28840ea0de7411",
+    "url": "/snake/static/css/main.2df7c62b.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/snake/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "07395b28840ea0de7411",
+    "url": "/snake/static/js/main.d837eecb.chunk.js"
+  },
+  {
+    "revision": "28fbb7debeb67f585d77",
+    "url": "/snake/static/js/runtime~main.ccc81976.js"
+  }
+]);
